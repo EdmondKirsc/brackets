6dae73b48cb730b9d9e5d566b18d09ac97095e74
EdmondKirsc <arzhanovpavel@mail.ru>
2021-03-12 11:31:25 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let result = [];
+  let strArr = str.split(""); //разбиваем строку на массив из скобок
+  if (strArr.length % 2 === 1) {
+    return false;
+  }
+  for (let i = 0; i < strArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if ((strArr[i] == bracketsConfig[j][1] && result[result.length-1] == bracketsConfig[j][0]))  {
+        result.pop();
+      }
+      else if (strArr[i] == bracketsConfig[j][0]) {
+        result.push(strArr[i]);
+      } 
+    }
+  }
+
+  if (result.length == 0) {
+    return true;
+  } else {
+    return false
+  };
 }
+
+
+
+
+
+
+
+
+
+
+
+
